aa2bb3a03f8b9879c764c4e6c1f16b2963b9a48b
wllalrx1314 <wllalrx1314@DESKTOP-5Q79TCR>
2018-08-16 20:38:32 +0800

    添加项目文件。

---
+++ FirstApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("please input your name:");
+            name=Console.ReadLine();
+            Console.WriteLine("hello,{0}!",name);
+
+        }
+    }
+}
